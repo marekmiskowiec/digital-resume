f120b9c6d959897df7b14c9cc926a5639a562d01
Marek Tomasz Miśkowiec <miskowiec.ma@gmail.com>
2024-08-28 21:24:03 +0200

    fix path v2

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -3,8 +3,8 @@ let icon = document.getElementById("icon");
 icon.onclick = () => {
   document.body.classList.toggle("light-theme");
   if (document.body.classList.contains("light-theme")) {
-    icon.src = "/assets/images/moon.png";
+    icon.src = "./assets/images/moon.png";
   } else {
-    icon.src = "/assets/images/sun.png";
+    icon.src = "./assets/images/sun.png";
   }
 };
